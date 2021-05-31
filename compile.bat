20fc8520fc82d9f80dffb0aae9ec291d28c8df0e
Metgraph <loydmajk61@gmail.com>
2021-05-31 17:22:30 +0200

    clean cache

--- compile.bat
+++
@@ -1,2 +0,0 @@
-g++ "Gioco\src\main\com\*.cpp" -I "C:\Users\edoza\Documents\programmazione\C++\SFML-2.5.1\include" -L "C:\Users\edoza\Documents\programmazione\C++\SFML-2.5.1\lib" -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -o "bin/gioco.exe"
-pause
