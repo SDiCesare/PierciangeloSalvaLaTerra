a95e3d3c85838515d0df30d1f18a093fa58ad6c5
SDiCesare <SDiCesare@users.noreply.github.com>
2021-05-31 17:46:05 +0200

    Revert "added comments to Entity, Fixed #2"
    This reverts commit 2f6d3c146d2f02c48dc6a3a43e491d7829a98d80.

--- Gioco/src/main/com/Entity.cpp
+++ Gioco/src/main/com/Entity.cpp
@@ -1,11 +1,9 @@
 #include "Entity.hpp"
 
-// Entity get a sprite that will be the entity represented in the game
 Entity::Entity(sf::Sprite sprite){
     entitySprite = sprite;
 }
 
-// get a vector composed by 2 float, it will move the sprite in that position
 Entity::setPosition(sf::Vector2f v2f){
     entitySprite.setOrigin(v2f);
 }
