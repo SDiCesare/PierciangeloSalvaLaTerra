05a4a46e704de850be72f98d58779d1f0994bd9c
Metgraph <loydmajk61@gmail.com>
2021-06-05 12:50:59 +0200

    new class

---
+++ Gioco/src/main/com/HUD/HealthBarNum.cpp
@@ -0,0 +1,53 @@
+#include "HealthBarNum.h"
+
+#include <string>
+
+HealthBarNum::HealthBarNum() {
+    healthNum.setFont(font);
+    healthNum.setCharacterSize(16);
+}
+
+HealthBarNum::HealthBarNum(int maxHealth, const sf::Vector2f& size) : HealthBar(maxHealth, size) {
+    healthNum.setCharacterSize(16);
+    updateNum();
+}
+
+void HealthBarNum::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    sf::Transform t = getTransform();
+    target.draw(rect, t);
+    target.draw(healthNum, t);
+}
+
+bool HealthBarNum::takeDamage(int damage) {
+    bool ret = HealthBar::takeDamage(damage);
+    updateNum();
+    return ret;
+}
+
+void HealthBarNum::setHealthStat(int health, int maxHealth){
+    HealthBar::setHealthStat(health, maxHealth);
+    updateNum();
+}
+
+void HealthBarNum::setHealth(int health){
+    HealthBar::setHealth(health);
+    updateNum();
+}
+
+void HealthBarNum::setMaxHealth(int maxHealth, bool propotionalHealth){
+    HealthBar::setMaxHealth(maxHealth);
+    updateNum();
+}
+
+void HealthBarNum::setBar(const sf::Vector2f& size){
+    HealthBar::setBar(size);
+    healthNum.setPosition(rect.getLocalBounds().width - healthNum.getLocalBounds().width, 0.f);
+}
+
+void HealthBarNum::setFont(sf::Font& font){
+    healthNum.setFont(font);
+}
+
+void HealthBarNum::updateNum() {
+    healthNum.setString(std::to_string(health) + "/" + std::to_string(maxHealth));
+}
---
+++ Gioco/src/main/com/HUD/HealthBarNum.h
@@ -0,0 +1,23 @@
+#ifndef HealthBarNum_h
+#define HealthBarNum_h
+#include "HealthBar.h"
+
+class HealthBarNum : public HealthBar {
+    public:
+        HealthBarNum();
+        HealthBarNum(int maxHealth, const sf::Vector2f& size);
+        bool takeDamage(int damage) override;
+        void setHealthStat(int health, int maxHealth) override;
+        void setHealth(int health) override;
+        void setMaxHealth(int maxHealth, bool propotionalHealth = true) override;
+        void setBar(const sf::Vector2f& size) override;
+        void setFont(sf::Font& font);
+
+    private:
+        sf::Font font;
+        sf::Text healthNum;
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+        void updateNum();
+};
+
+#endif
